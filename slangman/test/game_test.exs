57d28a305a73cafaa8e3a085a6f19c879c378dc5
Gez Jones <apostrophedottilde@Gezs-MacBook-Pro.local>
2019-10-14 10:40:21 +0100

    Implemented and tested basic functionality which tests whether a move has already been made or not.

--- slangman/test/game_test.exs
+++ slangman/test/game_test.exs
@@ -15,8 +15,25 @@ defmodule GameTest do
   test "state isn't changed for :won or :lost game" do
     for state <- [:won, :lost] do
       game = Game.new_game() |> Map.put(:game_state, state)
-      assert {^game, _} = Game.make_move(game, "x")
+      assert {^game, _} = Game.make_move(game, "X")
     end
+  end
+
+  test "first occurrence of letter is not already used" do
+    game = Game.new_game()
+
+    { game, _tally } = Game.make_move(game, "X")
+    assert game.game_state != :already_used
+  end
+
+  test "second occurrence of letter is not already used" do
+    game = Game.new_game()
+
+    { game, _tally } = Game.make_move(game, "X")
+    assert game.game_state != :already_used
+
+    { game, _tally } = Game.make_move(game, "X")
+    assert game.game_state == :already_used
   end
 
 end
